3da5a3aebd52cf74057a8d3d08cad7403b5c008b
ctague <ctague@bren.ucsb.edu>
2010-03-29 20:49:47 +0000

    chagned this to call compute_soil_water_potential since they are currently the same - avoids dublication and makes assumption of soil-predawn-leaf equilibrium clear

--- rhessys/hydro/compute_lwp_predawn.c
+++ rhessys/hydro/compute_lwp_predawn.c
@@ -28,8 +28,10 @@
 /*	double	Tsoil - (deg C)				*/
 /*	double	LWP_min_spring - (Mpa)			*/
 /*	double	LWP_stom_closure - (Mpa)		*/
-/*  	double  p1 - curve parameter 1                  */
-/*      double  p2 - curve parameter 2                  */
+/*  	double  psi_air_entry - curve parameter 1                  */
+/*      double  pore_size_index - curve parameter 2                  */
+/*	double  p3					*/
+/*	double  p4					*/
 /*      double  p_0 - porosity		                */
 /*      double  p porosity decay                  	*/
 /*	double	sat_deficit- (m)			*/
@@ -40,44 +42,8 @@
 /*							*/
 /*	DESCRIPTION					*/
 /*							*/
-/*	We make the assumption that the roots are sitting in a		*/
-/*	soil or moss column.  We assume that the roots do not		*/
-/*	extend through more than one column.  We then assume that	*/
-/*	the rooting field capacity is the amount of water that		*/
-/*	can be held (at steady state drainage) within the region of	*/
-/*	the column occupied by roots ABOVE the saturate zone  		 */
-/*																*/
-/*																*/
-/*	PROGRAMMER NOTES											*/
-/*																*/
-/*	Sept 24, 1997 RAF					*/
-/*	New code.						*/
-/*																*/
-/*	Oct 15 1997 RAF						*/
-/*	water unavailable to roots should have been in terms 	*/
-/*	of sat_deficit_z and not rooting_depth			*/
-/*																*/
-/*	Oct 15 1997 RAF						*/
-/*	Changed condition where satdef_z==0 giving no water	*/
-/*	stresss to sat_def_z<=0 (i.e. also if ponding)		*/
-/*																*/
-/*	Oct 15 1997 RAF						*/
-/*	LWP_predawn from Brooks Corey was in m water tension.	*/
-/*	converted to Mpa.					*/
-/*								*/
-/*	Nov 6, 1997 RAF						*/
-/*	Simpli8fied so that unsat zone soil moisture is assumed	*/
-/*	to be distributed such that unsat zone tension is the	*/
-/*	same everwhere.						*/
-/*								*/
-/*	Nov 27 , 1997 RAF					*/
-/*	Cleared bug where we were first checking if 		*/
-/*	unsat storage is zero before checking if sat deficit was*/
-/*	negative.  This meant that if there was ponding there	*/
-/*	was stomatal closure - while this may be true we prefer	*/
-/*	to assume that the ponding is statistical (in low areas)*/
-/*	and is currently indicative of NO stress to leaves.	*/
-/*	So we have switched the order of these checks.		*/
+/* assume lwp is the same as soil water potential 	*/
+/* in the rooting zone					*/
 /*--------------------------------------------------------------*/
 #include <stdio.h>
 #include "rhessys.h"
@@ -88,8 +54,10 @@ double	compute_lwp_predawn(
 							double	Tsoil,
 							double	LWP_min_spring,
 							double	LWP_stom_closure,
-							double	p1,
-							double 	p2,
+							double	psi_air_entry,
+							double 	pore_size_index,
+							double  p3,
+							double  p4,
 							double  p_0,
 							double  p,
 							double	S)
@@ -97,76 +65,38 @@ double	compute_lwp_predawn(
 	/*--------------------------------------------------------------*/
 	/*	Local function declaration				*/
 	/*--------------------------------------------------------------*/
+
+	double  compute_soil_water_potential(
+		int,
+		int,
+		double,
+		double,
+		double,
+		double,
+		double,
+		double,
+		double,
+		double,
+		double,
+		double);
+
 	/*--------------------------------------------------------------*/
 	/*	Local variable definition.				*/
 	/*--------------------------------------------------------------*/
 	double	LWP_predawn;
-	/*--------------------------------------------------------------*/
-	/*      Make sure p and p_0 are non zero.                       */
-	/*--------------------------------------------------------------*/
-	p = max(p,0.00000001);
-	p_0 = max(p_0,0.00000001);
-	/*--------------------------------------------------------------*/
-	/*	compute soil water storage				*/
-	/*--------------------------------------------------------------*/
-	/*--------------------------------------------------------------*/
-	/*	We assume that roots BELOW the water table do not 	*/
-	/*	uptake water actively.					*/
-	/*--------------------------------------------------------------*/
-	/*--------------------------------------------------------------*/
-	/*	We assume that water is distribued in the unsat zone 	*/
-	/*	so that relative effective moisture content is uniform	*/
-	/*	with z.  Then no matter how deep the roots are they 	*/
-	/*	still sense the same unsat zone suction.		*/
-	/*	Note that the other extreme, that water fills the 	*/
-	/*	unsat zone from bottom up, will make predawn LWP 	*/
-	/*	sensitive to rooting depth in the unsat zone.  However	*/
-	/*	for wet areas or areas where there is hydraulic lift 	*/
-	/*	the current assumption may be useful on a daily time stp*/
-	/*--------------------------------------------------------------*/
-	/*--------------------------------------------------------------*/
-	/*	Now we use Clapp and Hornberger relationship 	 	*/
-	/*	or the Can Genuchten relation ship to			*/
-	/*	define psi-theta curve					*/
-	/*--------------------------------------------------------------*/
-	/*--------------------------------------------------------------*/
-	/*	If stot == 0 we set LWP_predawn to its maximum value	*/
-	/*	this is a problem but if unsat_storage is zero there	*/
-	/*	is actually lots of water				*/
-	/*--------------------------------------------------------------*/
-	if (S > 1.0) {
-		LWP_predawn = LWP_min_spring;
-	}
-	else{
-		/*--------------------------------------------------------------*/
-		/*	Compute predawn LWP without soil temp adjustment.	*/
-		/*	This is converted from m water tension to MPa using	*/
-		/*	1m water tension = 10000 Pa				*/
-		/*			= 0.01 Mpa				*/
-		/*--------------------------------------------------------------*/
-		if ( curve == 1 ){
-			LWP_predawn = min(-1.0 * 0.01 * ( p1
-				* pow ( S , -1/p2) ), LWP_min_spring);
-		}
-		else{
-			if (S > ZERO)
-				LWP_predawn = min( -1.0 * 0.01 * p1
-					* pow( pow(1/S,1/(1-1/p2)) -	1 , 1/p2 )
-					, LWP_min_spring);
-			else LWP_predawn = LWP_stom_closure;
-		}
-	}
-	/*--------------------------------------------------------------*/
-	/*	Increase LWP is soil is cold.				*/
-	/*	(I am not sure where the factor of 2.0 came from	*/
-	/*--------------------------------------------------------------*/
-	/*
-	if ( Tsoil < 0.0000000001 )
-		LWP_predawn = LWP_predawn * 2.0;
-	*/
-	/*--------------------------------------------------------------*/
-	/*	Limit the lowest LWP to that of stomatal closure	*/
-	/*--------------------------------------------------------------*/
-	LWP_predawn = max(LWP_predawn, LWP_stom_closure);
+
+	LWP_predawn = compute_soil_water_potential(verbose_flag,
+		curve,
+		Tsoil,
+		LWP_min_spring,
+		LWP_stom_closure,
+		psi_air_entry,
+		pore_size_index,
+		p3,
+		p4,
+		p_0,
+		p,
+		S);
+
 	return(LWP_predawn);
 } /*end compute_lwp_predawn)*/
