8a07a54cd172cfcb537920fb4e3bd77f9b6f96b9
esgarcia <garcia@geog.ucsb.edu>
2012-06-25 20:18:00 +0000

    previous check-in did not have init/parse_alloc_flag.c

---
+++ rhessys/init/parse_alloc_flag.c
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------------*/
+/*                                                              */ 
+/*		parse_alloc_flag					*/
+/*                                                              */
+/*  NAME                                                        */
+/*		parse_alloc_flag					*/
+/*   	                                                        */
+/*                                                              */
+/*  SYNOPSIS                                                    */
+/* int parse_alloc_flag( char *)					*/
+/*                                                              */
+/*  OPTIONS                                                     */
+/*                                                              */
+/*	returns an integer value from vegetation type		*/
+/*	character string					*/
+/*                                                              */
+/*                                                              */
+/*  PROGRAMMER NOTES                                            */
+/*--------------------------------------------------------------*/
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "rhessys.h"
+
+int	parse_alloc_flag( char *input_string)
+{
+	/*------------------------------------------------------*/
+	/*	Local Function Declarations.						*/
+	/*------------------------------------------------------*/
+	
+	/*------------------------------------------------------*/
+	/*	Local Variable Definition. 							*/
+	/*------------------------------------------------------*/
+	int alloc_flag;
+	if ((strncmp(input_string,"STATIC",6) == 0) ||
+		(strncmp(input_string, "static",6) == 0) )
+		alloc_flag = CONSTANT;
+	else if ((strncmp(input_string,"DYNAMIC",7) == 0) ||
+		(strncmp(input_string, "allocamic",7) == 0) )
+		alloc_flag = DYNAMIC;
+	else if ((strncmp(input_string,"CONSTANT",8) == 0) ||
+		(strncmp(input_string, "constant",8) == 0) )
+		alloc_flag = CONSTANT;
+	else if ((strncmp(input_string,"WARING",6) == 0) ||
+		(strncmp(input_string, "waring",6) == 0) )
+		alloc_flag = WARING;
+	else if ((strncmp(input_string,"DICKENSON",9) == 0) ||
+		(strncmp(input_string, "dickenson",9) == 0) )
+		alloc_flag = DICKENSON;
+	else {
+		fprintf(stderr,"\n FATAL ERROR - parse_alloc_flag");
+		fprintf(stderr,"\n Flag must be stati or allocamic and is %s" , input_string);
+		exit(EXIT_FAILURE);
+	}
+	return(alloc_flag);
+}/*end parse_alloc_flag.c*/
