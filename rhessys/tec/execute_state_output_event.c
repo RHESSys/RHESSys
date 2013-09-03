54f3f23bf7f354a9651444f467d274abe5d95237
Janet Choate <jchoate@foxtrot.esm.ucsb.edu>
2013-09-03 12:19:35 -0700

    Fixed bug where output_current_state still output a header (so was not compatible with new worldfile format)

--- rhessys/tec/execute_state_output_event.c
+++ rhessys/tec/execute_state_output_event.c
@@ -69,62 +69,7 @@ void	execute_state_output_event(
 		fprintf(stderr,"FATAL ERROR: in execute_state_output_event.\n");
 		exit(EXIT_FAILURE);
 	}
-	/*--------------------------------------------------------------*/
-	/*	output world information									*/
-	/*--------------------------------------------------------------*/
-	fprintf(outfile, "%-30ld %s", current_date.year, "current_year");
-	fprintf(outfile, "\n%-30ld %s", current_date.month, "current_month");
-	fprintf(outfile, "\n%-30ld %s", current_date.day, "current_day");
-	fprintf(outfile, "\n%-30ld %s", current_date.hour, "current_hour");
-	fprintf(outfile, "\n%-30ld %s", end_date.year, "end_year");
-	fprintf(outfile, "\n%-30ld %s", end_date.month, "end_month");
-	fprintf(outfile, "\n%-30ld %s", end_date.day, "end_day");
-	fprintf(outfile, "\n%-30ld %s", end_date.hour, "end_hour");
-	fprintf(outfile, "\n%-30ld %s", world[0].defaults[0].num_basin_default_files,
-		"num_basin_default_files");
-	for (i=0; i<world[0].defaults[0].num_basin_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].basin_default_files[i],
-			"basin_default_file");
-	}
-	fprintf(outfile, "\n%-30ld %s",
-		world[0].defaults[0].num_hillslope_default_files,
-		"num_hillslope_default_files");
-	for (i=0; i<world[0].defaults[0].num_hillslope_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].hillslope_default_files[i],
-			"hillslope_default_file");
-	}
-	fprintf(outfile, "\n%-30ld %s", world[0].defaults[0].num_zone_default_files,
-		"num_zone_default_files");
-	for (i=0; i<world[0].defaults[0].num_zone_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].zone_default_files[i],
-			"zone_default_file");
-	}
 
-	fprintf(outfile, "\n%-30ld %s", world[0].defaults[0].num_soil_default_files,
-		"num_soil_default_files");
-	for (i=0; i<world[0].defaults[0].num_soil_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].soil_default_files[i],
-			"soil_default_file");
-	}
-	fprintf(outfile, "\n%-30ld %s", world[0].defaults[0].num_landuse_default_files,
-		"num_landuse_default_files");
-	for (i=0; i<world[0].defaults[0].num_landuse_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].landuse_default_files[i],
-			"landuse_default_file");
-	}
-	fprintf(outfile, "\n%-30ld %s",
-		world[0].defaults[0].num_stratum_default_files,
-		"num_stratum_default_files");
-	for (i=0; i<world[0].defaults[0].num_stratum_default_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].stratum_default_files[i],
-			"basin_default_file");
-	}
-	fprintf(outfile, "\n%-30ld %s", world[0].num_base_station_files,
-		"num_base_stations");
-	for (i=0; i<world[0].num_base_station_files; i++){
-		fprintf(outfile, "\n%-30s %s", world[0].base_station_files[i],
-			"base_stations_file");
-	}
 	fprintf(outfile, "\n%-30ld %s", world[0].ID,
 		"world_id");
 	fprintf(outfile, "\n%-30ld %s", world[0].num_basin_files,
