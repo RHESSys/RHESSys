RHESSys/RHESSys

commit e424ffb7d97b7f8f7ae2f7dffb94348de4109bd5

- change_world.c: fixed critical bug where ID[5] was being set to   patchID instead of vegid for calls to find_tlevel, which caused all   patches to not be found.
- makefile: set -g as default instead of -O to make debugging possible

- main.c: Added 'mask' command line option.  If not supplied, will try
  to use a raster mask named 'mask'

- main.c: enforced proper use of EXIT_FAILURE and EXIT_SUCCESS

- blender.h: changed MAXS from 80 to 1024 to allow lairead to be
  called using absolute paths

util/GRASS/lairead/change_world.c
@@ -103,7 +103,7 @@ change_world(oldworld, redefine, flow_table, num_patches)
 		ID[2] = flow_table[i].hillID;
 		ID[3] = flow_table[i].zoneID;
 		ID[4] = flow_table[i].patchID;
-		ID[5] = flow_table[i].patchID;
+		ID[5] = flow_table[i].vegid;
 
 		printf("\n processing patchID %d", flow_table[i].patchID);
 		flow_table[i].worldlink = find_tlevel(tlevel[0].children[0], ID, 1);
@@ -114,11 +114,9 @@ change_world(oldworld, redefine, flow_table, num_patches)
 
 			if (flow_table[i].lai < 1.0) {
 				flow_table[i].worldlink[0].valuelist[7] = 1.0/flow_table[i].veglink[0].sla;
-				}
-
-			else
+			} else {
 				flow_table[i].worldlink[0].valuelist[7] = flow_table[i].lai/flow_table[i].veglink[0].sla;
-					
+			}
 					
 			flow_table[i].worldlink[0].valuelist[4] = 0.0;
 			flow_table[i].worldlink[0].valuelist[5] = 0.0;

util/GRASS/lairead/blender.h
@@ -9,7 +9,7 @@
 
 #define CELL   30		/* pixel dimensions in metres */
 
-#define MAXS   80		/* size of filename string    */
+#define MAXS   1024		/* size of filename string    */
 
 #define FPTR_SIZE 8		/* number of elements in each pixel
 				   for the fractional pointer	*/

util/GRASS/lairead/main.c
@@ -50,7 +50,7 @@ main(int argc, char *argv[])
     char name[MAXS];
 
     /* Raster names */
-    char *rnLAI, *rnVegid, *rnPatch, *rnHill, *rnZone;
+    char *rnLAI, *rnVegid, *rnPatch, *rnHill, *rnZone, *rnMask;
 
     /* set pointers for images */
     int	     *vegid;
@@ -130,9 +130,15 @@ main(int argc, char *argv[])
     patch_raster_opt->required = NO;
     patch_raster_opt->description = "Patch raster map name";
 
+    struct Option* mask_raster_opt = G_define_option();
+    mask_raster_opt->key = "mask";
+    mask_raster_opt->type = TYPE_STRING;
+    mask_raster_opt->required = NO;
+    mask_raster_opt->description = "Mask raster map name. If none supplied 'mask' will be used.";
+
     // Parse GRASS arguments
     if (G_parser(argc, argv))
-        exit(1);
+        exit(EXIT_FAILURE);
 
     // Get values from GRASS arguments
     fnWorld    = worldfile_name_opt->answer;
@@ -143,24 +149,25 @@ main(int argc, char *argv[])
     rnZone        = zone_raster_opt->answer;
     rnHill        = hill_raster_opt->answer;
     rnPatch       = patch_raster_opt->answer;
+    rnMask		  = mask_raster_opt->answer;
 
     /* allometric file */
     if ( (fac = fopen(fnAllom, "r")) == NULL) {
         printf("cannot open allometric ratio file \'%s\'\n", fnAllom);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     if ( (fdWorld = fopen(fnWorld, "r")) == NULL) {
         printf("cannot open world file \'%s\' for reading.\n", fnWorld);
         usage();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* redefine world */
     if ( (fdRedefWorld = fopen(fnRedefWorld, "w")) == NULL) {
         printf("cannot open new world \'%s\' file for output.\n", fnRedefWorld);
         usage();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* open some diagnostic output files */
@@ -169,7 +176,7 @@ main(int argc, char *argv[])
     if ( (out1 = fopen(name, "w")) == NULL) {
         printf("cannot open diagnostic file %s for writing\n", name);
         usage();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* allocate and input map images */
@@ -180,7 +187,12 @@ main(int argc, char *argv[])
     }
 
     mask = (int *) calloc(maxr*maxc, sizeof(int));
-    mask = (int*)raster2array("mask", &patch_header, NULL, NULL, CELL_TYPE);
+
+    if (rnMask != NULL) {
+    	mask = (int*)raster2array(rnMask, &mask_header, NULL, NULL, CELL_TYPE);
+    } else {
+    	mask = (int*)raster2array("mask", &mask_header, NULL, NULL, CELL_TYPE);
+    }
 
     //printf("Reading %s raster map\n", rnPatch);
     patch = (int *) calloc(maxr*maxc, sizeof(int));
@@ -256,15 +268,12 @@ main(int argc, char *argv[])
     change_world(fdWorld, fdRedefWorld, flow_table, num_patches);
 
     printf("\n Finished LAIread \n\n");
-    exit(0);
+    exit(EXIT_SUCCESS);
 } /* end lairead.c */
 
 
 void usage(void) {
-    printf("\nOPTIONS\n\n");
-    printf("\t-allom\tname of allometric ratios file\n");
-    printf("\t-old\told worldfile name\n");
-    printf("\t-redef\tnew redefine worldfile name\n");
+	G_usage();
 
     return;
 }

util/GRASS/lairead/makefile
@@ -3,8 +3,8 @@ CC  = gcc
 # For grassIO library, we need to know where createflowpaths lives 
 # grassIO should be refactored into its own location in the source tree 
 CFPATH = ../../../cf
-CFLAGS = -O -I$(GISBASE)/include -I$(CFPATH)/include
-#CFLAGS = -g -I$(GISBASE)/include -I$(CFPATH)/include
+#CFLAGS = -O -I$(GISBASE)/include -I$(CFPATH)/include
+CFLAGS = -g -I$(GISBASE)/include -I$(CFPATH)/include
 RHESSYS_BIN = /usr/local/bin
 GIS_LIBS = -lgrass_gis
 

commit 64efd2122d35d3f2cb23cc2bbba54125663bb707

Fixed bug introduced in commit e424ffb7d97b7f8f7ae2f7dffb94348de4109bd5 where spatial extent of strata were set to vedID map instead of patchID map

util/GRASS/lairead/change_world.c
@@ -137,7 +137,7 @@ change_world(char* oldworld_name, FILE *oldworld, FILE *redefine,
 		ID[2] = flow_table[i].hillID;
 		ID[3] = flow_table[i].zoneID;
 		ID[4] = flow_table[i].patchID;
-		ID[5] = flow_table[i].vegid;
+		ID[5] = flow_table[i].patchID;
 
 		printf("\n processing patchID %d", flow_table[i].patchID);
 		flow_table[i].worldlink = find_tlevel(tlevel[0].children[0], ID, 1);