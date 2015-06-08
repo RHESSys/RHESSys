e50367ca9d65a4226d3705f9294b050a546b2fbd
Maureen Kennedy <mkenn@uw.edu>
2015-06-08 15:05:33 -0700

    added flag for ignition using veg

--- rhessys/init/construct_fire_defaults.c
+++ rhessys/init/construct_fire_defaults.c
@@ -145,6 +145,8 @@ struct fire_default *construct_fire_defaults(
   		printf("veg_k1: %lf\n",default_object_list[i].veg_k1);
 		default_object_list[i].veg_k2=getDoubleParam(&paramCnt, &paramPtr, "veg_k2", "%lf", 10, 1);
 		printf("veg_k2: %lf\n",default_object_list[i].veg_k2);
+ 		default_object_list[i].veg_ign=getDoubleParam(&paramCnt, &paramPtr, "veg_ign", "%d", 10, 1);
+		printf("veg_ign: %d\n",default_object_list[i].veg_ign);
  
 /*--------------------------------------------------------------*/
 		/*		Close the ith default file.								*/
