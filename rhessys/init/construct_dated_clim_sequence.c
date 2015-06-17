46e3ed81b26917ba6e710265f25dcce6bebe2c6a
Xiaoli Chen <haier.cxl@gmail.com>
2015-06-17 12:11:18 -0700

    clean comments

--- rhessys/init/construct_dated_clim_sequence.c
+++ rhessys/init/construct_dated_clim_sequence.c
@@ -273,9 +273,6 @@ struct clim_event_sequence construct_dated_clim_sequence(
 
 
 	printf("number_day with hourly precipitation in the input file= %d\nnumber_hour = number_day * 24 = %d\ninx=%d\nnum_records=%d\n",num_days,num_hours,inx,num_records);
-	//test
-	/*     for (i=0; i<inx; i++){
-		printf("%d %d %d %d %f\n",events.seq[i].edate.year,events.seq[i].edate.month,events.seq[i].edate.day,events.seq[i].edate.hour,events.seq[i].value);
-	}*/
+
 	return(events);
 } /*end construct_dated_clim_sequence*/
