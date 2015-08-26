5131a4ff1e2df2f5bf01aa2217cb823cfca1e9f6
Xiaoli Chen <haier.cxl@gmail.com>
2015-08-26 16:33:51 -0700

    hydro/compute_varbased_flow.c this time it is compute_varbased_flow.c

--- rhessys/hydro/compute_varbased_flow.c
+++ rhessys/hydro/compute_varbased_flow.c
@@ -86,16 +86,6 @@ double	compute_varbased_flow(
 	fs_spill = patch[0].soil_defaults[0][0].fs_spill;
 	fs_percolation = patch[0].soil_defaults[0][0].fs_percolation;
 
-	//patch[0].soil_defaults[0][0].soil_depth * patch[0].soil_defaults[0][0].porosity_0 * 0.99 *
-	//	   (1 - patch[0].soil_defaults[0][0].sat_store);
-	/*if(patch[0].ID==33575){
-	    printf("threshold = %f,sat_store = %f, sat_deficit_capacity=%f\n",
-	    threshold,
-	    patch[0].soil_defaults[0][0].sat_store,
-	    n_0 * p * (1 - exp(-soil_depth/p))
-	    );
-	}
-	*/
 	normal[0] = 0;
 	normal[1] = 0.253;
 	normal[2] = 0.524;
@@ -147,37 +137,11 @@ double	compute_varbased_flow(
 		    abovthre_flow = (transmissivity[didx]-thre_flow) * fs_spill; // fs_spill default value is 1 
       		    flow = abovthre_flow + thre_flow * fs_percolation;  // fs_percolation defaults = 1
 		}
-		/*if (patch[0].ID==33575){
-		  printf("sat_deficit=%f, threshold=%f,thre_flow=%f,abovethre_flow=%f,flow=%f,trasn=%f,fs_spill=%f\n",
-			patch[0].sat_deficit,
-			threshold,
-			thre_flow,
-			abovthre_flow,
-			flow,
-			transmissivity[didx],
-			fs_spill);
-		}*/
+
 	
 	}
 
 	flow = flow*gamma;
-/*	//the following code is for testing only
-	if(patch[0].ID==33575){
-	 printf("fs_spillover=%f, fs_percolation=%f, fs_threshold=%f\n",
-	      patch[0].soil_defaults[0][0].fs_spill,
-	      patch[0].soil_defaults[0][0].fs_percolation, 
-	      patch[0].soil_defaults[0][0].fs_threshold);
-	 
-	  
-	  printf("sat_store=%f, sat_deficit=%f, flow=%f, didx=%d, didthr=%d,maxtransimisivity=%f\n",
-	      patch[0].soil_defaults[0][0].sat_store, 
-	      patch[0].sat_deficit,
-	      flow,
-	      didx, 
-	      didthr,
-	      transmissivity[0]);
-	}
 
-*/
 	  return(flow);
 } /*compute_varbased_flow*/
