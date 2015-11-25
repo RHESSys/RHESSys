4dc43c4cdf0f3d5b97d24a3cc89b7ab127d884f4
Brian Miles <selimnairb@gmail.com>
2015-11-24 21:01:50 -0500

    Re-enable ACK reading

--- rhessys/output/output_patch.cc
+++ rhessys/output/output_patch.cc
@@ -174,25 +174,23 @@ void	output_patch(struct  command_line_object * command_line,
 		}
 
 		//printf("RHESSys.output_patch(): get response...");
-		if (0) {
-			char response[2];
-			rc = zmq_recv(world_output_files->patchdbmq_requester, response, 1, 0);
-			if (rc == -1) {
-				if (errno == EFSM) {
-					printf("output_patch: zeromq: operation cannot be performed on this socket at the moment due to the socket not being in the appropriate state.\n");
-				} else {
-					printf("output_patch: zeromq returned, output_patch:185: %d, exiting...\n", errno);
-				}
-				exit(EXIT_FAILURE);
+		char response[2];
+		rc = zmq_recv(world_output_files->patchdbmq_requester, response, 1, 0);
+		if (rc == -1) {
+			if (errno == EFSM) {
+				printf("output_patch: zeromq: operation cannot be performed on this socket at the moment due to the socket not being in the appropriate state.\n");
+			} else {
+				printf("output_patch: zeromq returned, output_patch:185: %d, exiting...\n", errno);
 			}
-			//printf("done\n");
+			exit(EXIT_FAILURE);
+		}
+		//printf("done\n");
 
-			//response[1] = 0;
-			if (response[0] != 'A') {
-				printf("output_patch: expected patchdbmq server to return %s, but received: %s, exiting...\n",
-						"A", response);
-				exit(EXIT_FAILURE);
-			}
+		//response[1] = 0;
+		if (response[0] != 'A') {
+			printf("output_patch: expected patchdbmq server to return %s, but received: %s, exiting...\n",
+					"A", response);
+			exit(EXIT_FAILURE);
 		}
 
 	} else {
