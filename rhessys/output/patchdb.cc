2e6af41a5a693f98f8a1940a2946119d2b90e1d5
Brian Miles <selimnairb@gmail.com>
2015-11-28 22:41:43 -0500

    Set cass_cluster_set_core_connections_per_host(*cluster, 2), cass_cluster_set_max_connections_per_host(*cluster, 8);

--- rhessys/output/patchdb.cc
+++ rhessys/output/patchdb.cc
@@ -91,8 +91,8 @@ void init_patchdb(char* hostname,
 
 //	cass_cluster_set_pending_requests_high_water_mark(*cluster, 1024);
 //	cass_cluster_set_pending_requests_low_water_mark(*cluster, 512);
-//	cass_cluster_set_core_connections_per_host(*cluster, 2);
-//	cass_cluster_set_max_connections_per_host(*cluster, 8);
+	cass_cluster_set_core_connections_per_host(*cluster, 2);
+	cass_cluster_set_max_connections_per_host(*cluster, 8);
 
 	CassFuture* connect_future = cass_session_connect(*session, *cluster);
 
