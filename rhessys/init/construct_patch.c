1c61d916b545071d822f15a0434623fe06cc1e2d
mkenn <mkenn@uw.edu>
2016-07-21 12:56:10 -0700

    add understory et and pet to construct patch

--- rhessys/init/construct_patch.c
+++ rhessys/init/construct_patch.c
@@ -262,6 +262,8 @@ struct patch_object *construct_patch(
 	if (command_line[0].firespread_flag == 1) {
 		patch[0].fire.et = 0.0;
 		patch[0].fire.pet = 0.0;
+		patch[0].fire.understory_et = 0;
+		patch[0].fire.understory_pet = 0
 		// here set fire.understory_et and fire.understory_pet = 0;
 		}	
 	/*--------------------------------------------------------------*/
