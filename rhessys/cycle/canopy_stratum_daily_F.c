a272127b7bbbe97963763aeae416b28970fbcdb8
mkenn <mkenn@uw.edu>
2016-07-26 08:26:37 -0700

    fixed another bug

--- rhessys/cycle/canopy_stratum_daily_F.c
+++ rhessys/cycle/canopy_stratum_daily_F.c
@@ -1856,7 +1856,7 @@ void	canopy_stratum_daily_F(
 	//	{	
 	//		printf("Found an understory stratum!\n");
 			patch[0].fire.understory_et = (patch[0].fire_defaults[0][0].ndays_average*patch[0].fire.understory_et  +  
-			(stratum[0].transpiration_sat_zone + stratum[0].transpiration_unsat_zone)/(patch[0].fire_defaults[0][0].ndays_average + 1); 
+			(stratum[0].transpiration_sat_zone + stratum[0].transpiration_unsat_zone))/(patch[0].fire_defaults[0][0].ndays_average + 1); 
 		//	+ stratum[0].evaporation))/ // MCK: look at et without the evaporation part
 			
 
