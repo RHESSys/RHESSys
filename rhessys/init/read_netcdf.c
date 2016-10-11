ed4315bf450474bf2fea93d70b2aba91b32c8d77
ejhanan <hanan@lifesci.ucsb.edu>
2016-10-11 11:03:08 -0700

    updates to repeated clim for netcdf spinup

--- rhessys/init/read_netcdf.c
+++ rhessys/init/read_netcdf.c
@@ -186,6 +186,8 @@ Nov. 17, 2011
   int shouldRepeat = 0;
 
   shouldRepeat = 1;// XXX artificial flag for testing; remove and get rid of subsequent instances
+  
+  //fprintf(stderr, "days being measured: read_duration %d, nday %d\n", read_duration, nday );
 
   if((startday<days[0] || (duration+startday) > days[nday-1])){
     if( clim_repeat_flag == 0 || shouldRepeat != 1 ) {
@@ -195,24 +197,69 @@ Nov. 17, 2011
       free(lont);	
       return -1;
     }else{
-      read_duration = nday;
+      //read_duration = nday;
       shouldRepeat = 1;
     }
   }
   
-  start[0] = startday-days[0]+day_offset;		//netcdf 4.1.3 problem: there is 1 day offset
+  int daysNeeded = duration - ( nday - startday-days[0]+day_offset );
+  fprintf( stderr, "start day:%d daysNeeded:%d\n", startday-days[0]+day_offset, daysNeeded );
+
+ 
+  // if shouldRepeat, start reading from 0 index
+  start[0] = shouldRepeat ? 0 : startday-days[0]+day_offset;		//netcdf 4.1.3 problem: there is 1 day offset
   start[1] = idlat;           //lat
   start[2] = idlont;
-  count[0] = read_duration;
+  
+  // if shouldRepeat, read all the available data (nday stores the total number of available days)
+  count[0] = shouldRepeat ? nday : nday - startday-days[0]+day_offset;//read_duration;
   count[1] = 1;
   count[2] = 1;
   /***Read netcdf data***/ 
-  if ((retval = nc_get_vara_float(ncid,temp_varid,start,count,&data[0]))){
-    free(days);
-    free(lat);
-    free(lont);
-    ERR(retval);
+  float * allActualData;
+ 
+  if( !shouldRepeat ) {
+    if ((retval = nc_get_vara_float(ncid,temp_varid,start,count,&data[0]))){
+      free(days);
+      free(lat);
+      free(lont);
+      ERR(retval);
+    }
+  }else{
+    fprintf( stderr, "READING ALL AVAILABLE DATA\n" );
+    allActualData  = (float *) alloc(nday * sizeof(float),"allActualData","get_netcdf_var_timeserias");
+    if ((retval = nc_get_vara_float(ncid,temp_varid,start,count,&allActualData[0]))){
+      free(days);
+      free(lat);
+      free(lont);
+      ERR(retval);
+    }
   }
+  fprintf( stderr, "WE HAVE READ NETCDF\n" );
+
+/*
+ * 1. Read all data from netcdf and store in separate array (allActualData). DONE
+ * 2. Determine start index to read from allAcutalData.
+ * 3. memcpy start index to end of allActualData into passed data[] array (assuming we want that many days)
+ * 4. we are now at the end of the allActualData array. Starting from the beginning of allActualData, we try
+ *    to find matching month/day combinations for subsequent required data points. So for each further required data point, we:
+ *    - increment a counter
+ *    - check the value held in allActualData at the counter position
+ *    - if the month and day of the value is the same as the month and data we need for our next data point, copy the value into our target data[] array.
+ *    - else repeat this process
+ *
+ * 5. For each step in the above loop, check to see if we've filled in the required amount of data; if so, return. Also check to see if we've passed the end
+ *    of the allActualData array... if so, reset counter to 0.
+ *
+ * 6. Need to read clim_repeat_flag from command line to determine whether any of this takes place.
+ *
+ * Variables:
+ *    nday : the total number of days in the actual netcdf file
+ *    allActualData: the entire dataset from the netcdf file, irrespective of how much data is requested by the user
+ *    startday: the starting index to read from allActualData... the first date requested when this function is called.
+ *    duration: the number of days of requested data... 
+ *    data: an array passed as an argument to this function to be populated with the requested netcdf data
+ */
 
   if( shouldRepeat ) {
     // XXX put Naomi's code in here
@@ -220,22 +267,36 @@ Nov. 17, 2011
     struct date target_date;
     struct date curr_date;
     int start_date = startday - days[0] + day_offset;
+    int amountToMemCopy = nday - start_date;
 
-    for( int i = nday; i < duration; i++ ) {
+    memcpy( &allActualData[ start_date ], &data[0], amountToMemCopy );
+/* 
+ * now we should have all the data from the start date to the end of the actual data copied over. 
+ * next comes looping through and artifically creating the other data points.
+ */   
+
+    fprintf( stderr, "start_date %d, startday %d, durationRequest %d, days in dataset %d\n", start_date, startday, duration, nday );
+
+    for( int i = amountToMemCopy; i < duration; i++ ) {
       int target_fnd = 0;
       int j = 0;
-
+      
+      // our next data point to populate
       target_date = caldat( start_date + i );
 
-      while( target_fnd == 0 && j < i ) {	
+      while( target_fnd == 0 && j < nday ) {	
           curr_date = caldat( start_date + j );
-          if ((curr_date.month == target_date.month) 
-              && (curr_date.day == target_date.day)) target_fnd=1;
-          j = j+1;
-        }
-      if (j < i) {
-        *(data+i) = *(data+j);
+          if ((curr_date.month == target_date.month) && (curr_date.day == target_date.day)) {
+            target_fnd = 1;
+          }
+          j++;
+      }
+
+      if( j < nday && target_fnd == 1 ) {
+        fprintf( stderr, "match! j: %d, i: %d\n", j, i );
+        data[i]  = allActualData[j];
       } else {
+        fprintf( stderr, "target_date.day %d, target_date.month %d, target_date.year %d\n", target_date.day, target_date.month, target_date.year ); 
         fprintf(stderr,"FATAL ERROR: in construct_clim_sequence\n");
         fprintf(stderr,"\n not enough data in base climate to repeat\n");
         exit(EXIT_FAILURE);
@@ -253,6 +314,9 @@ Nov. 17, 2011
   free(days);
   free(lat);
   free(lont);
+  if( shouldRepeat ) {
+    free( allActualData );
+  }
   return 0;
 }
 //_____________________________________________________________________________/
