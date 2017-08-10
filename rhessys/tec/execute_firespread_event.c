4f019a5f111ec5d2ac1bb74bf687dc12a627044c
mkenn <mkenn@uw.edu>
2017-08-09 20:05:48 -0700

    Minor edits

--- rhessys/tec/execute_firespread_event.c
+++ rhessys/tec/execute_firespread_event.c
@@ -247,6 +247,7 @@ void execute_firespread_event(
 	/* Call WMFire	 						*/
 	/*--------------------------------------------------------------*/
 	printf("calling WMFire: month %ld year %ld  cell res %lf  nrow %d ncol % d\n",current_date.month,current_date.year,command_line[0].fire_grid_res,world[0].num_fire_grid_row,world[0].num_fire_grid_col);
+// needs to return fire size, not just grid--create structure that includes fire size, or a 12-member array of fire sizes, and/or a tally of fires > 1000 acres
 	world[0].fire_grid=WMFire(command_line[0].fire_grid_res,world[0].num_fire_grid_row,world[0].num_fire_grid_col,current_date.year,current_date.month,world[0].fire_grid,*(world[0].defaults[0].fire));
  	printf("Finished calling WMFire\n");
 	/*--------------------------------------------------------------*/
