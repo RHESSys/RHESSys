6b0a8ce274f679725970413069718150bd1832f9
wburke24 <burke24@gmail.com>
2017-11-09 14:59:03 -0800

    renaming, weird git changes

--- RHESSysPreprocessing/R/fix_all_pits.R
+++ RHESSysPreprocessing/R/fix_all_pits.R
@@ -1,13 +1,13 @@
 # call fill_pit on all pits
 fix_all_pits<-function(flw){
   pits<-pit_list(flw)   #find all pits
-  
+
   if (length(pits)==1){
     return(flw)        #no pits
   }
   pits<-pits[-1]   #throw out lowest pit
   num_pits<-length(pits)
-  
+
   for (j in 1:num_pits){
     i<-pits[j]   #fill pits
     if (sum(flw[[i]]$Gamma_i)==0){
@@ -16,3 +16,4 @@ fix_all_pits<-function(flw){
   }
   return(flw)
 }
+#
