b417c57a8446074dac9766fec9c4f5223f8f2907
Erin Hanan <erin.hanan@wsu.edu>
2018-11-16 11:39:52 -0800

    accounting for standing dead grass in fuel

--- rhessys/tec/execute_firespread_event.c
+++ rhessys/tec/execute_firespread_event.c
@@ -159,7 +159,10 @@ void execute_firespread_event(
 
 					world[0].fire_grid[i][j].fuel_veg += (patch[0].canopy_strata[(patch[0].layers[layer].strata[c])][0].cover_fraction
 						* patch[0].canopy_strata[(patch[0].layers[layer].strata[c])][0].cs.leafc) *
-							patch_fire_grid[i][j].prop_patch_in_grid[p] ;
+							patch_fire_grid[i][j].prop_patch_in_grid[p] ;       
+              world[0].fire_grid[i][j].fuel_litter +=(patch[0].canopy_strata[(patch[0].layers[layer].strata[c])][0].cover_fraction
+                * patch[0].canopy_strata[(patch[0].layers[layer].strata[c])][0].cs.dead_leafc) *
+                patch_fire_grid[i][j].prop_patch_in_grid[p] ;
 					}
 				}
 //			printf("pixel veg and prop patch in grid: %lf\t%lf\n",world[0].fire_grid[i][j].fuel_veg,patch_fire_grid[i][j].prop_patch_in_grid[p]);
