83d26538070438321b9a38329b0d6ac33df3e18c
Naomi Tague <ctague@bren.ucsb.edu>
2019-02-13 16:35:04 -0800

    fixed bug where cstate_struct variable age was defined as integer but used as double - creating instability

--- rhessys/cn/allocate_annual_growth.c
+++ rhessys/cn/allocate_annual_growth.c
@@ -445,7 +445,7 @@ int allocate_annual_growth(				int id,
 
 		//printf("\n Resprouting stratum %d", id);
 		cs->num_resprout += 1;
-		cs->age = 0;
+		cs->age = 0.0;
 		cs->cpool = 0.0;
 		ns->npool = 0.0;
 		cs->leafc_store = epc.resprout_leaf_carbon;
@@ -514,7 +514,11 @@ int allocate_annual_growth(				int id,
 	} /* end if less than min_leaf_carbon */
 	else  {
 		 // cs->num_resprout = max(cs->num_resprout-1,0);	
-		 cs->age += 1;
+		 cs->age += cs->age + 1.0;
+		 if (cs->age > 10000000.0) { printf("\n\n Age has not been reset for %lf years Resetting to avoid numerical issues",
+						cs->age);
+				cs->age=1.0;
+			};
 		 }
 
 	/* update states variables */
