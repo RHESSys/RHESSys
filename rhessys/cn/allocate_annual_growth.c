d710b1057c2ef721a73367f8da4b72939618c77d
Naomi Tague <ctague@bren.ucsb.edu>
2019-02-18 13:12:58 -0800

    input of strata age had been commented out - reinserted so age properly initialized

--- rhessys/cn/allocate_annual_growth.c
+++ rhessys/cn/allocate_annual_growth.c
@@ -515,10 +515,11 @@ int allocate_annual_growth(				int id,
 	else  {
 		 // cs->num_resprout = max(cs->num_resprout-1,0);	
 		 cs->age += cs->age + 1.0;
-		 if (cs->age > 10000000.0) { printf("\n\n Age has not been reset for %lf years Resetting to avoid numerical issues",
+		 if (cs->age > 10000000.0) { 
+			printf("\n\n Age has not been reset for %lf years Resetting to avoid numerical issues",
 						cs->age);
 				cs->age=1.0;
-			};
+			}
 		 }
 
 	/* update states variables */
