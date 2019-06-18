c2fd595a9cbde214ea07ea46633e3bd0b0e6b393
Naomi Tague <ctague@bren.ucsb.edu>
2019-06-18 12:55:00 -0700

    Added a xylem conductance (hydraulic failure) model; to turn on set epc.LWP_gxylem_min to reasonable potential; currently -999.0 so conductance doesn't change

---
+++ rhessys/hydro/compute_xylem_conductance.c
@@ -0,0 +1,62 @@
+/*--------------------------------------------------------------*/
+/* 								*/
+/*			compute_xylem_conductance	*/
+/*								*/
+/*	compute_xylem_conductance - computes conductance to H20	*/
+/*				of a single mean vascular stratum		*/
+/*								*/
+/*	NAME							*/
+/*	compute_xylem_conductance 				*/
+/*								*/
+/*	SYNOPSIS						*/
+/*	double	compute_xylem_conductance(			*/
+/*								*/
+/*	OPTIONS							*/
+/*								*/
+/*	DESCRIPTION						*/
+/*	follows the TREES approach to reducing xylem conductance */
+/* 	due to low soil water potential and recovers based 	*/
+/*	on days after recovery of soil water above some 	*/
+/*	treshold value						*/
+/*      mackay et al (2015) Interdependence of hydraulic...	*/
+/*	Water Resources Research				*/
+/*								*/
+/*								*/
+/*	PROGRAMMER NOTES					*/
+/*								*/
+/*--------------------------------------------------------------*/
+#include <stdio.h>
+#include "rhessys.h"
+
+void	compute_xylem_conductance(
+											 double	LWP_predawn,
+											 struct epconst_struct epc,
+											 struct	canopy_strata_object *stratum)
+											 
+{
+	/*--------------------------------------------------------------*/
+	/*	Local function declaration				*/
+	/*--------------------------------------------------------------*/
+	/*--------------------------------------------------------------*/
+	/*	Local variable definition.				*/
+	/*--------------------------------------------------------------*/
+
+	double gxylem;
+
+
+	if (LWP_predawn < epc.LWP_gxylem_min) {
+	stratum[0].gxylem = epc.gxylem_max * exp(pow( (-1.0*LWP_predawn/epc.gxylem_bsat),epc.gxylem_csat));
+	}
+
+
+	if ((stratum[0].gxylem < epc.gxylem_max) && (LWP_predawn > epc.LWP_gxylem_min)) {
+		stratum[0].gxylem  += epc.gxylem_recovery_rate;
+	}
+	
+	stratum[0].gxylem = min(epc.gxylem_max, stratum[0].gxylem);
+	stratum[0].gxylem = max(stratum[0].gxylem,0.0);
+
+
+	
+	return;
+} /*end compute_xylem_conductance*/
