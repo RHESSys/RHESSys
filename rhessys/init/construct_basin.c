68fca1e6261d37301e79818c13e5b9dd489eaf8c
naomitague <ctague@bren.ucsb.edu>
2020-02-17 15:24:22 -0800

    in construct basin the parallel creation of a routing list for the TOPMODEL case was mixing hillsloep and basins in an incorherent way - fixed to just be hillslope

--- rhessys/init/construct_basin.c
+++ rhessys/init/construct_basin.c
@@ -416,7 +416,9 @@ struct basin_object *construct_basin(
   } else { // command_line[0].routing_flag != 1
     // For TOPMODEL mode, make a dummy route list consisting of all patches
     // in the hillslope, in no particular order.
-    hillslope->route_list = construct_topmodel_patchlist(hillslope);
+    int h;
+    for (h=0; h < basin[0].num_hillslopes; h++) 
+   		basin[0].hillslopes[h]->route_list = construct_topmodel_patchlist(basin[0].hillslopes[h]);
   }
 
   /*--------------------------------------------------------------*/
