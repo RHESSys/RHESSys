81a5f8fadeda0010eaf6d3df72ccd807b83dc3ea
Brian Miles <selimnairb@gmail.com>
2020-08-21 21:31:01 -0500

    Move bool typedef to types.h to allow including from headers that can't include rhessys.h due to circular references

---
+++ rhessys/include/types.h
@@ -0,0 +1,19 @@
+/*
+ * types.h
+ *
+ *  Created on: Aug 21, 2020
+ *      Author: miles
+ */
+
+#ifndef INCLUDE_TYPES_H_
+#define INCLUDE_TYPES_H_
+
+/*----------------------------------------------------------*/
+/*      Define types                                        */
+/*----------------------------------------------------------*/
+typedef short bool;
+static const short true = 1;
+static const short false = 0;
+
+
+#endif /* INCLUDE_TYPES_H_ */
