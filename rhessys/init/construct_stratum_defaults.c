364ed595c6a6a836bad2e1e482655d8e863ac398
naomitague <ctague@bren.ucsb.edu>
2020-08-24 14:37:19 -0700

    changed root distribution parameter to an appropriate default value from Arora table

--- rhessys/init/construct_stratum_defaults.c
+++ rhessys/init/construct_stratum_defaults.c
@@ -302,8 +302,8 @@ struct stratum_default *construct_stratum_defaults(
 		default_object_list[i].epc.litter_gsurf_slope = getDoubleParam(&paramCnt, &paramPtr, "epc.litter_gsurf_slope", "%lf", 0.0, 1);
 		default_object_list[i].epc.litter_gsurf_intercept = getDoubleParam(&paramCnt, &paramPtr, "epc.litter_gsurf_intercept", "%lf", 100000000, 1);
 		default_object_list[i].epc.coef_CO2 = getDoubleParam(&paramCnt, &paramPtr, "epc.coef_CO2", "%lf", 1.0, 1);
-		default_object_list[i].epc.root_growth_direction = getDoubleParam(&paramCnt, &paramPtr, "epc.root_growth_direction", "%lf", 0.8, 1);
-		default_object_list[i].epc.root_distrib_parm = getDoubleParam(&paramCnt, &paramPtr, "epc.root_distrib_parm", "%lf", 8.0, 1);
+		default_object_list[i].epc.root_growth_direction = getDoubleParam(&paramCnt, &paramPtr, "epc.root_growth_direction", "%lf", 0.7, 1);
+		default_object_list[i].epc.root_distrib_parm = getDoubleParam(&paramCnt, &paramPtr, "epc.root_distrib_parm", "%lf", 1.25, 1);
 		default_object_list[i].epc.max_root_depth = getDoubleParam(&paramCnt, &paramPtr, "epc.max_root_depth", "%lf", 9999.0, 1);
 		default_object_list[i].epc.crown_ratio = getDoubleParam(&paramCnt, &paramPtr, "epc.crown_ratio", "%lf", 0.6, 1);
 		if (epc->veg_type != TREE) 
