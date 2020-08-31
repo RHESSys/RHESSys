3d4b90a5d3295f1c2468c583d160cda4b6f13610
Brian Miles <selimnairb@gmail.com>
2020-08-30 20:26:46 -0500

    Stub out daily output filter output

---
+++ rhessys/include/output_filter/output_filter_output.h
@@ -0,0 +1,16 @@
+/*
+ * output_filter_output.h
+ *
+ *  Created on: Aug 30, 2020
+ *      Author: miles
+ */
+
+#ifndef INCLUDE_OUTPUT_FILTER_OUTPUT_FILTER_OUTPUT_H_
+#define INCLUDE_OUTPUT_FILTER_OUTPUT_FILTER_OUTPUT_H_
+
+#include "output_filter.h"
+
+void output_filter_output_daily(OutputFilter *filters);
+void output_filter_output_patch_daily(OutputFilter *filters);
+
+#endif /* INCLUDE_OUTPUT_FILTER_OUTPUT_FILTER_OUTPUT_H_ */
---
+++ rhessys/output_filter/output_filter_output.c
@@ -0,0 +1,10 @@
+#include "output_filter/output_filter_output.h"
+
+void output_patch_daily(OutputFilter *filters) {
+
+}
+
+void output_filter_output_daily(OutputFilter *filters) {
+	fprintf(stderr, "output_filter_output_daily(): Where filtered output will happen...\n");
+}
+
