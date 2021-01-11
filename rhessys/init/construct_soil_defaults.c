b26973373f03fd9ca7bfbbd6b15baadd1b06a966
naomitague <ctague@bren.ucsb.edu>
2021-01-11 14:44:58 -0800

    m and m_z should linked by porosity - done now and allow chanage in soil defaults to impact m,k and m,k veriticals

--- rhessys/init/construct_soil_defaults.c
+++ rhessys/init/construct_soil_defaults.c
@@ -101,8 +101,10 @@ struct soil_default *construct_soil_defaults(
 		default_object_list[i].ID = 			getIntParam(&paramCnt, &paramPtr, "patch_default_ID", "%d", 3, 1); 
 		default_object_list[i].theta_psi_curve = 	getIntParam(&paramCnt, &paramPtr, "theta_psi_curve", "%d", 1.0, 1);
 		default_object_list[i].Ksat_0 = 		getDoubleParam(&paramCnt, &paramPtr, "Ksat_0", "%lf", 3.0, 1);
+		default_object_list[i].Ksat_0_v = 		getDoubleParam(&paramCnt, &paramPtr, "Ksat_0_v", "%lf", 3.0, 1);
 		default_object_list[i].cap_rise_max = 		getDoubleParam(&paramCnt, &paramPtr, "cap_rise_max", "%lf", 0.8, 1);
 		default_object_list[i].m = 			getDoubleParam(&paramCnt, &paramPtr, "m", "%lf", 0.12, 1);
+		default_object_list[i].m_v = 			getDoubleParam(&paramCnt, &paramPtr, "m_v", "%lf", 0.12, 1);
 		default_object_list[i].porosity_0 = 		getDoubleParam(&paramCnt, &paramPtr, "porosity_0", "%lf", 0.435, 1);
 		default_object_list[i].porosity_decay = 	getDoubleParam(&paramCnt, &paramPtr, "porosity_decay", "%lf", 4000.0, 1);
 		default_object_list[i].p3 = 			getDoubleParam(&paramCnt, &paramPtr, "P3", "%lf", 0.0, 1); // param name upper case in param file
@@ -110,7 +112,6 @@ struct soil_default *construct_soil_defaults(
 		default_object_list[i].psi_air_entry = 		getDoubleParam(&paramCnt, &paramPtr, "psi_air_entry", "%lf", 0.218, 1);
 		default_object_list[i].psi_max = 		getDoubleParam(&paramCnt, &paramPtr, "psi_max", "%lf", 0.01, 1);
 		default_object_list[i].soil_depth = 		getDoubleParam(&paramCnt, &paramPtr, "soil_depth", "%lf", 200.0, 1);
-		default_object_list[i].m_z = 			getDoubleParam(&paramCnt, &paramPtr, "m_z", "%lf", 0.4, 1);
 		default_object_list[i].detention_store_size = 	getDoubleParam(&paramCnt, &paramPtr, "detention_store_size", "%lf", 0.0, 1);
 		default_object_list[i].deltaz = 		getDoubleParam(&paramCnt, &paramPtr, "deltaZ", "%lf", 1.0, 1); // param name contains uppercase "Z" in param file
 		default_object_list[i].active_zone_z = 		getDoubleParam(&paramCnt, &paramPtr, "active_zone_z", "%lf", 5.0, 1);
@@ -181,34 +182,48 @@ struct soil_default *construct_soil_defaults(
 		default_object_list[i].fs_percolation =	getDoubleParam(&paramCnt, &paramPtr, "fs_percolation", "%lf", 1, 1);
 		default_object_list[i].fs_threshold = 	getDoubleParam(&paramCnt, &paramPtr, "fs_threshold", "%lf", 0.2, 1);
 		
+;
 		/*--------------------------------------------------------------*/
 		/*	vertical soil m and K are initized using soil default	*/
 		/*	but sensitivity analysis -s is not applied to them	*/
 		/*      use -sv to change these parameters			*/
 		/*--------------------------------------------------------------*/
+		/*
 			default_object_list[i].m_v = default_object_list[i].m;
 			default_object_list[i].mz_v = default_object_list[i].m_z;
-			default_object_list[i].Ksat_0_v = default_object_list[i].Ksat_0;
+			default_object_list[i].Ksat_0_v = default_object_list[i].Ksat_0; */
 
 
 		/*--------------------------------------------------------------*/
-		/* sensitivity adjustment of vertical drainage  soil paramters	*/
+		/* sensitivity adjustment of vertical drainage  soil paramters is different	*/
+		/*--------------------------------------------------------------*/
+		/*--------------------------------------------------------------*/
+		/* sensitivity adjustment of soil drainage paramters		*/
 		/*--------------------------------------------------------------*/
 		if (command_line[0].vsen_flag > 0) {
 				default_object_list[i].m_v *= command_line[0].vsen[M];
 				default_object_list[i].mz_v *= command_line[0].vsen[M];
 				default_object_list[i].Ksat_0_v *= command_line[0].vsen[K];
-		}
 
+				default_object_list[i].m *= command_line[0].sen[M];
+				default_object_list[i].Ksat_0 *= command_line[0].sen[K];
+		} else {
+
+		if (command_line[0].sen_flag > 0) {
 		/*--------------------------------------------------------------*/
 		/* sensitivity adjustment of soil drainage paramters		*/
 		/*--------------------------------------------------------------*/
-		if (command_line[0].sen_flag > 0) {
 				default_object_list[i].m *= command_line[0].sen[M];
-				default_object_list[i].m_z *= command_line[0].sen[M];
+				default_object_list[i].m_v *= command_line[0].sen[M];
 				default_object_list[i].Ksat_0 *= command_line[0].sen[K];
+				default_object_list[i].Ksat_0_v *= command_line[0].sen[K];
 				default_object_list[i].soil_depth *= command_line[0].sen[SOIL_DEPTH];
 		}
+		}
+
+
+		default_object_list[i].m_z = 	default_object_list[i].m/default_object_list[i].porosity_0;
+		default_object_list[i].mz_v = 	default_object_list[i].m_v/default_object_list[i].porosity_0;
 
 		/*--------------------------------------------------------------*/
 		/*      calculate water_equivalent depth of soil                */
