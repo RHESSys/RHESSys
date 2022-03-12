25826bae200d54d139b865b53015f8bab4e80c66
renjianning <renjianning@gmail.com>
2022-03-12 03:11:47 -0500

    add seasonality 8 and 9

--- rhessys/hydro/compute_patch_family_routing.c
+++ rhessys/hydro/compute_patch_family_routing.c
@@ -177,12 +177,12 @@ void compute_patch_family_routing(struct zone_object *zone,
             //TH is lower, most of the time is off; TH is higher, on all the time ; default is on the time
             //patch daily F, line 614, patch[0].rain_throughfall = zone[0].rain + irrigation; unit is m
             //update drainage land line 420 control MSR on or off; wet period, MSR is off
-            if (current_date.month >=7 && current_date.month <=10 && patches[0].canopy_strata[0][0].defaults[0][0].epc.hot_spot == 1) { //dry season must off
+            if (current_date.month >=7 && current_date.month <=8 && patches[0].canopy_strata[0][0].defaults[0][0].epc.hot_spot == 1) { //dry season must on to rout water
                 zone[0].patch_families[pf][0].patches[i][0].landuse_defaults[0][0].sh_g = 0.05; //make it small but not that small, no MSR makes hotspot always saturated
                 zone[0].patch_families[pf][0].patches[i][0].landuse_defaults[0][0].sh_l = 0.9; // if no hotspot no loss no gain, the hotspot no gain no loss too
             }
 
-            else if (sm_mean > patches[0].soil_defaults[0][0].diff_th  &&
+           else if (sm_mean > patches[0].soil_defaults[0][0].diff_th  &&
                 patches[0].sat_deficit > 1 &&
                 patches[0].canopy_strata[0][0].defaults[0][0].epc.hot_spot == 1)  // when there is precipitation, MSR off, when no precip, dry, MSR on, hotspot dry too
             { // wet period, MSR is off
