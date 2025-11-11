aca2668e176194b78737c9c0aac0977103eb660e
Ojas Sarup <4738915+osarup@users.noreply.github.com>
2025-11-11 10:52:20 -0800

    Fixed calc_resolution declarations

--- rhessys/init/construct_netcdf_header.c
+++ rhessys/init/construct_netcdf_header.c
@@ -25,7 +25,7 @@
 #define FLT_MAX 1000000000
 #endif
 
-double calc_resolution(const bool geographic_unit,const struct  base_station_object **basestations, const int station_numbers); 
+double calc_resolution(bool geographic_unit, struct  base_station_object **basestations, int station_numbers); 
 #ifdef LIU_NETCDF_READER
 /*Get the station numbers from station file                         */
 int get_netcdf_station_number(char *base_station_filename)
@@ -254,7 +254,7 @@ typedef struct Location{
     double y;
 } Location;
 //_____________________________________________________________________
-double calc_resolution(const bool geographic_unit,const struct  base_station_object **basestations, const int station_numbers)
+double calc_resolution(bool geographic_unit, struct  base_station_object **basestations, int station_numbers)
 {
     Location *sites  = (Location*)calloc(station_numbers, sizeof(Location));
     for (int i = 0; i < station_numbers; i++) {
